29a17fb489dc8d9b1c0f75d28ca85ec1b97d9425
flo26gz <flo26gz@gmail.com>
2013-01-15 15:43:48 +0100

    commit du projet

---
+++ Snake/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snake
+{
+    class Game
+    {
+        private int key_up = 0x00;
+        private int key_down = 0x01;
+        private int key_right = 0x02;
+        private int key_left = 0x03;
+  
+
+    }
+}
---
+++ Snake/SnakeLink.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snake
+{
+    class SnakeLink
+    {
+        private int posX;
+        private int poxY;
+        private int dir;
+        private int size;
+    }
+}
