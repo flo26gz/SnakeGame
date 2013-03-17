d6f159f8a6ab944ff7a313b9afd97d15a6f8054f
flo26gz <flo26gz@gmail.com>
2013-03-17 23:04:40 +0100

    Changement d'architecture pour la fenêtre d'intro deplacement des composants personnalisés (ajoutés directement dans le code ) dans le Form1.cs et non plus dans le Form1.Designer.cs

--- Snake/Form1.Designer.cs
+++ Snake/Form1.Designer.cs
@@ -30,59 +30,80 @@ namespace Snake
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.mainIntroPanel = new System.Windows.Forms.Panel();
-            this.optionButton = new System.Windows.Forms.Button();
+            this.menuPanel = new System.Windows.Forms.Panel();
+            this.sousMenu2Panel = new System.Windows.Forms.Panel();
+            this.sousMenu3Panel = new System.Windows.Forms.Panel();
             this.playButton = new System.Windows.Forms.Button();
-            this.rulesButton = new System.Windows.Forms.Button();
+            this.optionButton = new System.Windows.Forms.Button();
+            this.creditsLabel = new System.Windows.Forms.Label();
+            this.ruleLabel = new System.Windows.Forms.Label();
+            this.gameLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuLabel = new System.Windows.Forms.Label();
             this.teamLabel2 = new System.Windows.Forms.Label();
             this.teamLabel1 = new System.Windows.Forms.Label();
             this.SnakeTitleLabel = new System.Windows.Forms.Label();
-            this.imgSnakePanel = new System.Windows.Forms.Panel();
-            this.scoreLabel = new System.Windows.Forms.Label();
-            this.mainGamePanel = new System.Windows.Forms.Panel();
-            this.snakePanel = new System.Windows.Forms.Panel();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contentRulesLabel = new System.Windows.Forms.Label();
             this.mainIntroPanel.SuspendLayout();
-            this.mainGamePanel.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuPanel.SuspendLayout();
+            this.sousMenu2Panel.SuspendLayout();
+            this.sousMenu3Panel.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // mainIntroPanel
             // 
-            this.mainIntroPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.mainIntroPanel.Controls.Add(this.pictureBox1);
-            this.mainIntroPanel.Controls.Add(this.optionButton);
-            this.mainIntroPanel.Controls.Add(this.playButton);
-            this.mainIntroPanel.Controls.Add(this.rulesButton);
+            this.mainIntroPanel.BackColor = System.Drawing.Color.Black;
+            this.mainIntroPanel.Controls.Add(this.menuPanel);
+            this.mainIntroPanel.Controls.Add(this.panel1);
             this.mainIntroPanel.Controls.Add(this.teamLabel2);
             this.mainIntroPanel.Controls.Add(this.teamLabel1);
             this.mainIntroPanel.Controls.Add(this.SnakeTitleLabel);
-            this.mainIntroPanel.Controls.Add(this.imgSnakePanel);
             this.mainIntroPanel.Location = new System.Drawing.Point(-1, -1);
             this.mainIntroPanel.Name = "mainIntroPanel";
-            this.mainIntroPanel.Size = new System.Drawing.Size(737, 500);
+            this.mainIntroPanel.Size = new System.Drawing.Size(737, 504);
             this.mainIntroPanel.TabIndex = 0;
             // 
-            // optionButton
+            // menuPanel
             // 
-            this.optionButton.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.optionButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.optionButton.ForeColor = System.Drawing.Color.Green;
-            this.optionButton.Location = new System.Drawing.Point(351, 218);
-            this.optionButton.Name = "optionButton";
-            this.optionButton.Size = new System.Drawing.Size(165, 40);
-            this.optionButton.TabIndex = 6;
-            this.optionButton.Text = "Options";
-            this.optionButton.UseVisualStyleBackColor = false;
+            this.menuPanel.BackColor = System.Drawing.Color.DarkGray;
+            this.menuPanel.Controls.Add(this.sousMenu2Panel);
+            this.menuPanel.Controls.Add(this.creditsLabel);
+            this.menuPanel.Controls.Add(this.ruleLabel);
+            this.menuPanel.Controls.Add(this.gameLabel);
+            this.menuPanel.Location = new System.Drawing.Point(59, 155);
+            this.menuPanel.Name = "menuPanel";
+            this.menuPanel.Size = new System.Drawing.Size(605, 237);
+            this.menuPanel.TabIndex = 8;
+            // 
+            // sousMenu2Panel
+            // 
+            this.sousMenu2Panel.BackColor = System.Drawing.Color.DimGray;
+            this.sousMenu2Panel.Controls.Add(this.sousMenu3Panel);
+            this.sousMenu2Panel.Location = new System.Drawing.Point(136, 21);
+            this.sousMenu2Panel.Name = "sousMenu2Panel";
+            this.sousMenu2Panel.Size = new System.Drawing.Size(442, 198);
+            this.sousMenu2Panel.TabIndex = 4;
+            // 
+            // sousMenu3Panel
+            // 
+            this.sousMenu3Panel.BackColor = System.Drawing.Color.Transparent;
+            this.sousMenu3Panel.Controls.Add(this.contentRulesLabel);
+            this.sousMenu3Panel.Controls.Add(this.playButton);
+            this.sousMenu3Panel.Controls.Add(this.optionButton);
+            this.sousMenu3Panel.Location = new System.Drawing.Point(14, 13);
+            this.sousMenu3Panel.Name = "sousMenu3Panel";
+            this.sousMenu3Panel.Size = new System.Drawing.Size(414, 172);
+            this.sousMenu3Panel.TabIndex = 7;
             // 
             // playButton
             // 
             this.playButton.BackColor = System.Drawing.SystemColors.ControlLight;
             this.playButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.playButton.ForeColor = System.Drawing.Color.Green;
-            this.playButton.Location = new System.Drawing.Point(351, 136);
+            this.playButton.Location = new System.Drawing.Point(5, 1);
             this.playButton.Name = "playButton";
             this.playButton.Size = new System.Drawing.Size(165, 40);
             this.playButton.TabIndex = 4;
@@ -91,17 +112,76 @@ namespace Snake
             this.playButton.Click += new System.EventHandler(this.playButton_Click);
             this.playButton.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
             // 
-            // rulesButton
+            // optionButton
             // 
-            this.rulesButton.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.rulesButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rulesButton.ForeColor = System.Drawing.Color.Green;
-            this.rulesButton.Location = new System.Drawing.Point(351, 302);
-            this.rulesButton.Name = "rulesButton";
-            this.rulesButton.Size = new System.Drawing.Size(165, 40);
-            this.rulesButton.TabIndex = 5;
-            this.rulesButton.Text = "Rules";
-            this.rulesButton.UseVisualStyleBackColor = false;
+            this.optionButton.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.optionButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optionButton.ForeColor = System.Drawing.Color.Green;
+            this.optionButton.Location = new System.Drawing.Point(5, 66);
+            this.optionButton.Name = "optionButton";
+            this.optionButton.Size = new System.Drawing.Size(165, 40);
+            this.optionButton.TabIndex = 6;
+            this.optionButton.Text = "Options";
+            this.optionButton.UseVisualStyleBackColor = false;
+            // 
+            // creditsLabel
+            // 
+            this.creditsLabel.AutoSize = true;
+            this.creditsLabel.BackColor = System.Drawing.Color.Transparent;
+            this.creditsLabel.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.creditsLabel.ForeColor = System.Drawing.Color.White;
+            this.creditsLabel.Location = new System.Drawing.Point(22, 115);
+            this.creditsLabel.Name = "creditsLabel";
+            this.creditsLabel.Size = new System.Drawing.Size(87, 25);
+            this.creditsLabel.TabIndex = 3;
+            this.creditsLabel.Text = "Credits";
+            this.creditsLabel.Click += new System.EventHandler(this.creditsLabel_Click);
+            // 
+            // ruleLabel
+            // 
+            this.ruleLabel.AutoSize = true;
+            this.ruleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.ruleLabel.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ruleLabel.ForeColor = System.Drawing.Color.White;
+            this.ruleLabel.Location = new System.Drawing.Point(22, 67);
+            this.ruleLabel.Name = "ruleLabel";
+            this.ruleLabel.Size = new System.Drawing.Size(70, 25);
+            this.ruleLabel.TabIndex = 2;
+            this.ruleLabel.Text = "Rules";
+            this.ruleLabel.Click += new System.EventHandler(this.ruleLabel_Click);
+            // 
+            // gameLabel
+            // 
+            this.gameLabel.AutoSize = true;
+            this.gameLabel.BackColor = System.Drawing.Color.DarkGreen;
+            this.gameLabel.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameLabel.ForeColor = System.Drawing.Color.White;
+            this.gameLabel.Location = new System.Drawing.Point(22, 21);
+            this.gameLabel.Name = "gameLabel";
+            this.gameLabel.Size = new System.Drawing.Size(74, 25);
+            this.gameLabel.TabIndex = 0;
+            this.gameLabel.Text = "Game";
+            this.gameLabel.Click += new System.EventHandler(this.gameLabel_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkGreen;
+            this.panel1.Controls.Add(this.menuLabel);
+            this.panel1.Location = new System.Drawing.Point(59, 114);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(128, 44);
+            this.panel1.TabIndex = 7;
+            // 
+            // menuLabel
+            // 
+            this.menuLabel.AutoSize = true;
+            this.menuLabel.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuLabel.ForeColor = System.Drawing.Color.White;
+            this.menuLabel.Location = new System.Drawing.Point(27, 10);
+            this.menuLabel.Name = "menuLabel";
+            this.menuLabel.Size = new System.Drawing.Size(69, 25);
+            this.menuLabel.TabIndex = 0;
+            this.menuLabel.Text = "Menu";
             // 
             // teamLabel2
             // 
@@ -109,7 +189,7 @@ namespace Snake
             this.teamLabel2.BackColor = System.Drawing.Color.Transparent;
             this.teamLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.teamLabel2.ForeColor = System.Drawing.Color.Silver;
-            this.teamLabel2.Location = new System.Drawing.Point(40, 432);
+            this.teamLabel2.Location = new System.Drawing.Point(96, 432);
             this.teamLabel2.Name = "teamLabel2";
             this.teamLabel2.Size = new System.Drawing.Size(191, 29);
             this.teamLabel2.TabIndex = 3;
@@ -121,7 +201,7 @@ namespace Snake
             this.teamLabel1.BackColor = System.Drawing.Color.Transparent;
             this.teamLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.teamLabel1.ForeColor = System.Drawing.Color.Silver;
-            this.teamLabel1.Location = new System.Drawing.Point(40, 371);
+            this.teamLabel1.Location = new System.Drawing.Point(380, 432);
             this.teamLabel1.Name = "teamLabel1";
             this.teamLabel1.Size = new System.Drawing.Size(224, 29);
             this.teamLabel1.TabIndex = 2;
@@ -133,61 +213,28 @@ namespace Snake
             this.SnakeTitleLabel.BackColor = System.Drawing.Color.Transparent;
             this.SnakeTitleLabel.Font = new System.Drawing.Font("Plantagenet Cherokee", 48.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.SnakeTitleLabel.ForeColor = System.Drawing.Color.Green;
-            this.SnakeTitleLabel.Location = new System.Drawing.Point(197, 19);
+            this.SnakeTitleLabel.Location = new System.Drawing.Point(297, 20);
             this.SnakeTitleLabel.Name = "SnakeTitleLabel";
             this.SnakeTitleLabel.Size = new System.Drawing.Size(214, 87);
             this.SnakeTitleLabel.TabIndex = 0;
             this.SnakeTitleLabel.Text = "Snake";
             // 
-            // imgSnakePanel
-            // 
-            this.imgSnakePanel.Location = new System.Drawing.Point(0, 0);
-            this.imgSnakePanel.Name = "imgSnakePanel";
-            this.imgSnakePanel.Size = new System.Drawing.Size(200, 100);
-            this.imgSnakePanel.TabIndex = 7;
-            // 
-            // scoreLabel
-            // 
-            this.scoreLabel.AutoSize = true;
-            this.scoreLabel.BackColor = System.Drawing.Color.Transparent;
-            this.scoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.scoreLabel.ForeColor = System.Drawing.Color.Green;
-            this.scoreLabel.Location = new System.Drawing.Point(28, 550);
-            this.scoreLabel.Name = "scoreLabel";
-            this.scoreLabel.Size = new System.Drawing.Size(123, 29);
-            this.scoreLabel.TabIndex = 2;
-            this.scoreLabel.Text = "Score : 0";
-            // 
-            // mainGamePanel
-            // 
-            this.mainGamePanel.BackColor = System.Drawing.Color.Azure;
-            this.mainGamePanel.Controls.Add(this.snakePanel);
-            this.mainGamePanel.Controls.Add(this.scoreLabel);
-            this.mainGamePanel.Location = new System.Drawing.Point(-1, 0);
-            this.mainGamePanel.Name = "mainGamePanel";
-            this.mainGamePanel.Size = new System.Drawing.Size(626, 594);
-            this.mainGamePanel.TabIndex = 0;
-            // 
-            // snakePanel
-            // 
-            this.snakePanel.BackColor = System.Drawing.Color.Gray;
-            this.snakePanel.Location = new System.Drawing.Point(-1, 0);
-            this.snakePanel.Name = "snakePanel";
-            this.snakePanel.Size = new System.Drawing.Size(560, 504);
-            this.snakePanel.TabIndex = 0;
-            // 
             // timer1
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // pictureBox1
+            // contentRulesLabel
             // 
-            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(58, 175);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(126, 93);
-            this.pictureBox1.TabIndex = 8;
-            this.pictureBox1.TabStop = false;
+            this.contentRulesLabel.AutoSize = true;
+            this.contentRulesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.contentRulesLabel.ForeColor = System.Drawing.Color.White;
+            this.contentRulesLabel.Location = new System.Drawing.Point(3, 1);
+            this.contentRulesLabel.Name = "contentRulesLabel";
+            this.contentRulesLabel.Size = new System.Drawing.Size(365, 160);
+            this.contentRulesLabel.TabIndex = 7;
+            this.contentRulesLabel.Text = "Rules :\r\n\r\n- The goal of the game is to have the highest score\r\n\r\n- If you touch " +
+                "the walls, this is the end of the game\r\n\r\nGood Luck !\r\n\r\n";
+            this.contentRulesLabel.Visible = false;
             // 
             // Form1
             // 
@@ -200,9 +247,13 @@ namespace Snake
             this.Text = "Snake";
             this.mainIntroPanel.ResumeLayout(false);
             this.mainIntroPanel.PerformLayout();
-            this.mainGamePanel.ResumeLayout(false);
-            this.mainGamePanel.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuPanel.ResumeLayout(false);
+            this.menuPanel.PerformLayout();
+            this.sousMenu2Panel.ResumeLayout(false);
+            this.sousMenu3Panel.ResumeLayout(false);
+            this.sousMenu3Panel.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -211,21 +262,20 @@ namespace Snake
 
         private System.Windows.Forms.Panel mainIntroPanel;
         private System.Windows.Forms.Label SnakeTitleLabel;
-        private System.Windows.Forms.Panel imgSnakePanel;
-        //main panel of the game window that contains the score  + the snakePanel
-        private System.Windows.Forms.Panel mainGamePanel;
-        //panel that contains our snake and apple (the game)
-        private System.Windows.Forms.Panel snakePanel;
         private System.Windows.Forms.Label teamLabel2;
         private System.Windows.Forms.Label teamLabel1;
-        private System.Windows.Forms.Label scoreLabel;
         private System.Windows.Forms.Button optionButton;
-        private System.Windows.Forms.Button rulesButton;
         private System.Windows.Forms.Button playButton;
         private System.Windows.Forms.Timer timer1;
-        //To get the choice of the user at the end of the game(messageBox)
-        private DialogResult res;
-        private PictureBox pictureBox1;
+        private Panel menuPanel;
+        private Panel panel1;
+        private Label menuLabel;
+        private Label creditsLabel;
+        private Label ruleLabel;
+        private Label gameLabel;
+        private Panel sousMenu2Panel;
+        private Panel sousMenu3Panel;
+        private Label contentRulesLabel;
       
 
 
--- Snake/Form1.cs
+++ Snake/Form1.cs
@@ -13,9 +13,22 @@ namespace Snake
     public partial class Form1 : Form
     {
         private Game game;
+        //panel that contains all the options and score for the snake (left side of the game)
+        private System.Windows.Forms.Panel toolPanel;
+        //main panel of the game window that contains the score  + the snakePanel
+        private System.Windows.Forms.Panel mainGamePanel;
+        //panel that contains our snake and apple (the game)
+        private System.Windows.Forms.Panel snakePanel;
+        //score
+        private System.Windows.Forms.Label scoreLabel;
+        //To get the choice of the user at the end of the game(messageBox)
+        private DialogResult res;
+
+
         public Form1()
         {
             InitializeComponent();
+           
             
         }
 
@@ -23,6 +36,7 @@ namespace Snake
         {
          //remove all the components from the intro menu
          Controls.Clear();
+         myComponents();
          //add the game panel to the window
          Controls.Add(this.mainGamePanel);
          //start a game
@@ -98,6 +112,90 @@ namespace Snake
 
             }
         }
+
+        public void myComponents()
+        {
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.scoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.ForeColor = System.Drawing.Color.Green;
+            this.scoreLabel.Location = new System.Drawing.Point(10, 10);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(123, 29);
+            this.scoreLabel.TabIndex = 2;
+            this.scoreLabel.Text = "Score : 0";
+            // 
+            // toolPanel
+            // 
+            this.toolPanel = new System.Windows.Forms.Panel();
+            this.toolPanel.BackColor = System.Drawing.Color.Wheat;
+            this.toolPanel.Controls.Add(this.scoreLabel);
+            this.toolPanel.Location = new System.Drawing.Point(-1, 0);
+            this.toolPanel.Name = "toolPanel";
+            this.toolPanel.Size = new System.Drawing.Size(178, 504);
+            this.toolPanel.TabIndex = 0;
+            // 
+            // snakePanel
+            // 
+            this.snakePanel = new System.Windows.Forms.Panel();
+            this.snakePanel.BackColor = System.Drawing.Color.Gray;
+            this.snakePanel.Location = new System.Drawing.Point(177, 0);
+            this.snakePanel.Name = "snakePanel";
+            this.snakePanel.Size = new System.Drawing.Size(560, 504);
+            this.snakePanel.TabIndex = 0;
+            // 
+            // mainGamePanel
+            // 
+            this.mainGamePanel = new System.Windows.Forms.Panel();
+            this.mainGamePanel.BackColor = System.Drawing.Color.Azure;
+            this.mainGamePanel.Controls.Add(this.snakePanel);
+            this.mainGamePanel.Controls.Add(this.toolPanel);
+            this.mainGamePanel.Location = new System.Drawing.Point(-1, 0);
+            this.mainGamePanel.Name = "mainGamePanel";
+            this.mainGamePanel.Size = new System.Drawing.Size(737, 504);
+            this.mainGamePanel.TabIndex = 0;
+
+
+
+        }
+
+        private void gameLabel_Click(object sender, EventArgs e)
+        {
+            this.gameLabel.BackColor = System.Drawing.Color.DarkGreen;
+            this.ruleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.playButton.Visible = true;
+            this.optionButton.Visible = true;
+            this.contentRulesLabel.Visible = false;
+        }
+
+        private void ruleLabel_Click(object sender, EventArgs e)
+        {
+            this.ruleLabel.BackColor = System.Drawing.Color.DarkGreen;
+            this.gameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.playButton.Visible = false;
+            this.optionButton.Visible = false;
+            this.contentRulesLabel.Visible = true;
+        }
+
+        private void creditsLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void removeColorBackground(object sender, EventArgs e)
+        {
+            this.gameLabel.BackColor = System.Drawing.Color.Transparent;
+        }
+
+        private void addColorBackground(object sender, EventArgs e)
+        {
+            this.gameLabel.BackColor = System.Drawing.Color.DarkGreen;
+        }
+
 
     }
 }
